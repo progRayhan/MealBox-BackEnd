eb7d312afa75d3f83ff8b01efcdd12a717deb952
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-02-22 12:13:49 +0600

    feat: meal_rate model created

--- meal_manager/admin.py
+++ meal_manager/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from meal_manager.models import MealRate
+
+
+@admin.register(MealRate)
+class MealRateAdmin(admin.ModelAdmin):
+    list_display = ("vendor_name", "vendor_phone", "staff", "meal_rate")
+    search_fields = ("vendor_name", "vendor_phone", "staff__name")
+    list_filter = ("staff",)
+    ordering = ("vendor_name",)
+    list_per_page = 30
--- meal_manager/models.py
+++ meal_manager/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from meal_user.models import Staff
+from _applibs.validators import PHONE_REGEX
+
+
+class MealRate(models.Model):
+    vendor_name = models.CharField(max_length=100)
+    vendor_phone = models.CharField(max_length=11, validators=[PHONE_REGEX], unique=True, null=True, blank=True)
+    staff = models.ForeignKey(Staff, on_delete=models.DO_NOTHING, related_name="staff")
+    meal_rate = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.vendor_name
+    
+    class Meta:
+        verbose_name = "Meal Rate"
+        verbose_name_plural = "Meal Rates"
+        db_table = "meal_rate"
