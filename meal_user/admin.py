6511a4e06fe4fc5547779e47188aeec90dc80539
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-02-22 11:35:33 +0600

    feat: employee model created

--- meal_user/admin.py
+++ meal_user/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from meal_user.models import Employee
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone_number", "email", "gender", "office", "status")
+    search_fields = ("name", "phone_number", "email")
+    list_filter = ("gender", "status", "office")
+    ordering = ("name",)
+    list_per_page = 30
--- meal_user/models.py
+++ meal_user/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+from _applibs.validators import PHONE_REGEX
+from _applibs.choice_fields import Gender, EmployeeStatus
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=11, validators=[PHONE_REGEX], unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    gender = models.CharField(
+        max_length=10, choices=Gender.choices, default=Gender.MALE, null=True, blank=True
+    )
+    office = models.CharField(max_length=200, null=True, blank=True)
+    status = models.CharField(
+        max_length=10, choices=EmployeeStatus.choices, default=EmployeeStatus.ACTIVE
+    )
+
+    def __str__(self):
+        return self.phone_number
+    
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employes"
+        db_table = "employee"
