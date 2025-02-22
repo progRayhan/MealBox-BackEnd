f4aa99deab710fda5e52ec24a12e9dc7909c951d
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-02-22 16:00:22 +0600

    feat: staff dashboard value displayed

--- meal_manager/views/staff_dashboard.py
+++ meal_manager/views/staff_dashboard.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 from meal_manager.models import MealTracker
+from meal_user.models import Staff
 
 
 class StaffDashboardView(View):
     def get(self, request):
         staff_phone = "01942531394" # Todo: get phone from token
 
-        staff_meals = MealTracker.objects.filter(staff__phone_number=staff_phone)
+        staff = Staff.objects.filter(phone_number=staff_phone).first()
+        staff_meals = MealTracker.objects.filter(staff=staff)
         context = {
-            "total_sales": 1500,
-            "order_completed": 150,
-            "total_employee": 50,
+            "total_sales": staff.total_sales,
+            "order_completed": staff.order_completed,
+            "total_employee": staff.total_employee,
             "staff_meals": staff_meals
         }
         return render(request, "dashboard.html", context)
