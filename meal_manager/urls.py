3a91833949b38d6591e21436299f4d26dca6564d
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-02-24 00:29:46 +0600

    feat: staff dashboard redesign

--- meal_manager/urls.py
+++ meal_manager/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
-from meal_manager.views.staff_dashboard import StaffDashboardView
+from meal_manager.views.staff_dashboard import DashboardView, OrdersView, MealsView, SettingsView
 
 urlpatterns = [
     path(
         route='dashboard/', 
-        view=StaffDashboardView.as_view(), 
-        name='staff_dashboard',
+        view=DashboardView.as_view(), 
+        name='dashboard',
+    ),
+    path(
+        route='orders/', 
+        view=OrdersView.as_view(), 
+        name='orders',
+    ),
+    path(
+        route='meals/', 
+        view=MealsView.as_view(), 
+        name='meals',
+    ),
+    path(
+        route='settings/', 
+        view=SettingsView.as_view(), 
+        name='settings',
     ),
 ]
